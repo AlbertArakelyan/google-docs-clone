ae0fbe1eda411139260333fb5e7eb0315bef43c6
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-02-17 07:40:43 +0400

    simply inserted tiptap editor

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -1,6 +1,17 @@
+'use client';
+
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 
 export const Editor = () => {
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: '<p>Hello world! 👋</p>',
+  });
+
   return (
-    <div></div>
+    <div>
+      <EditorContent editor={editor} />
+    </div>
   );
 };
