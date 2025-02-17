c263f78a22c2cc766621a2d56de7738eb03ac799
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-02-17 07:25:53 +0400

    put shadcn under eslint ignore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 const Home = () => {
   return (
     <div className="flex min-h-screen items-center justify-center">
-      <Button>Click me</Button>
+      Click <Link className="text-blue-500 underline" href="/documents/123"> here </Link> to go to document id 123
     </div>
   );
 };
