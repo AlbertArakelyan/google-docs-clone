162d380dc8a5ddbd31505c5ef89f4598e4946cd7
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-02-18 06:53:21 +0400

    added tiptap tasklist extension

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -2,6 +2,8 @@
 
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKit from '@tiptap/starter-kit';
+import TaskList from '@tiptap/extension-task-list';
+import TaskItem from '@tiptap/extension-task-item';
 
 export const Editor = () => {
   const editor = useEditor({
@@ -11,7 +13,13 @@ export const Editor = () => {
         class: 'focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10 cursor-text',
       },
     },
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      TaskList,
+      TaskItem.configure({
+        nested: true,
+      }),
+    ],
     content: '<p>Hello world! 👋</p>',
   });
 
