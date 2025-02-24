bd70f7e853b860f8f56bade9338ae806d206e53e
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-02-24 06:30:31 +0400

    added highlight extension

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -3,6 +3,7 @@
 import {
   BoldIcon,
   ChevronDownIcon,
+  HighlighterIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -86,6 +87,30 @@ const TextColorButton = () => {
   );
 };
 
+const HighlightColorButton = () => {
+  const { editor } = useEditorStore();
+
+  const value = editor?.getAttributes('highlight').color || '#ffffff';
+
+  const onChange = (color: ColorResult) => {
+    editor?.chain().focus().setHighlight({ color: color.hex }).run();
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button
+          className="h-7 min-w-7 shrink-0 flex flex-col items-center justify-center rounded-sm hover:bg-neutral-200/80 px-1.5 overflow-hidden text-sm"
+        >
+          <HighlighterIcon className="size-4" />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="p-0">
+        <SketchPicker color={value} onChange={onChange} />
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
 
 const HeadingLevelButton = () => {
   const { editor } = useEditorStore();
@@ -283,7 +308,7 @@ export const Toolbar = () => {
         />
       ))}
       <TextColorButton />
-      {/* TODO Highlight color */}
+      <HighlightColorButton />
       <Separator orientation="vertical" className="h-6 bg-neutral-300" />
       {/* TODO: Link */}
       {/* TODO: Image */}
