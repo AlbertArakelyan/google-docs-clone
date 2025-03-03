2bfb48e880fb42ed32bbb8e7658c1024b84c7ac2
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-03 07:09:49 +0400

    prepared navbar component for homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link';
 
+import { Navbar } from './navbar';
+
 const Home = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      Click <Link className="text-blue-500 underline" href="/documents/123"> here </Link> to go to document id 123
+    <div className="min-h-screen flex flex-col">
+      <div className="fiex fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+        <Navbar />
+      </div>
+      <div className="mt-16">
+        Click <Link className="text-blue-500 underline" href="/documents/123"> here </Link> to go to document id 123
+      </div>
     </div>
   );
 };
