82e7751a5550a483a6ba300143ad09c17da1289a
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-10 06:50:52 +0400

    added search params

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -6,8 +6,11 @@ import { SearchIcon, XIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
+import { useSearchParam } from '@/hooks/use-search-param';
+
 export const SearchInput = () => {
-  const [value, setValue] = useState('');
+  const [search, setSearch] = useSearchParam();
+  const [value, setValue] = useState(search);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -17,6 +20,13 @@ export const SearchInput = () => {
 
   const handleClear = () => {
     setValue('');
+    setSearch('');
+    inputRef.current?.blur();
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
     inputRef.current?.blur();
   };
 
@@ -24,6 +34,7 @@ export const SearchInput = () => {
     <div className="flex-1 flex items-center justify-center">
       <form
         className="relative max-w-[720px] w-full"
+        onSubmit={handleSubmit}
       >
         <Input
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)] bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white"
