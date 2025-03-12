8564ddb5f7619db77917a116be7dcf725e2162c3
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-12 07:07:17 +0400

    changed clerk reak auth with nextjs auth

--- src/components/ConvexClientProvider.tsx
+++ src/components/ConvexClientProvider.tsx
@@ -1,8 +1,8 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ClerkProvider, useAuth, SignIn } from '@clerk/clerk-react'; // @clerk/nextjs
-import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ClerkProvider, useAuth, SignIn } from '@clerk/nextjs';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
 import { ConvexReactClient, Authenticated, Unauthenticated, AuthLoading } from 'convex/react';
 
 import { FullScreenLoader } from './full-screen-loader';
@@ -18,7 +18,7 @@ export const ConvexClientProvider = ({ children }: { children: ReactNode }) =>{
         </Authenticated>
         <Unauthenticated>
           <div className="flex flex-col items-center justify-center min-h-screen">
-            <SignIn />
+            <SignIn routing="hash" />
           </div>
         </Unauthenticated>
         <AuthLoading>
