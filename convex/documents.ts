6beb38e1c84ea351918d8c40d0dea91090b74028
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-12 07:13:45 +0400

    created post method for documents

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,26 @@
-import { query } from './_generated/server';
+import { ConvexError, v } from 'convex/values';
+
+import { query, mutation } from './_generated/server';
+
+export const create = mutation({
+  args: {
+    title: v.optional(v.string()),
+    initialContent: v.optional(v.string()),
+  },
+  handler: async (ctx, args) => {
+    const ueser = await ctx.auth.getUserIdentity();
+
+    if (!ueser) {
+      throw new ConvexError('Unauthorized');
+    }
+
+    return await ctx.db.insert('documents', {
+      title: args.title ?? 'Untitled Document',
+      ownerId: ueser.subject,
+      initialContent: args.initialContent,
+    });
+  },
+});
 
 export const get = query({
   handler: async (ctx) => {
