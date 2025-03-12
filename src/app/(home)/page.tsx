caaf8f3e86628acf939b364388ab3e7177bf6c53
Albert Arakelyan <albertarakelyan1998@gmail.com>
2025-03-12 08:02:28 +0400

    created basic documents table structure

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { useQuery } from 'convex/react';
+import { usePaginatedQuery } from 'convex/react';
 import { Navbar } from './navbar';
 import { TemplatesGallery } from './templates-gallery';
+import { DocumentsTable } from './documents-table';
 
 import { api } from '../../../convex/_generated/api';
 
+
 const Home = () => {
-  const documents = useQuery(api.documents.get);
+  const { results, status, loadMore } = usePaginatedQuery(api.documents.get, {}, { initialNumItems: 5 });
 
   if (document === undefined) {
     return <div>Loading...</div>;
@@ -20,9 +22,11 @@ const Home = () => {
       </div>
       <div className="mt-16">
         <TemplatesGallery />
-        {documents?.map((document) => (
-          <div key={document._id}>{document.title}</div>
-        ))}
+        <DocumentsTable
+          documents={results}
+          loadMore={loadMore}
+          status={status}
+        />
       </div>
     </div>
   );
